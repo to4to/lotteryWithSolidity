d2d682aeb3e002072c41e7bfee5818916b982bb5
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-01-21 23:54:13 +0530

    Fix bug in login functionality***
    ***Refactor code for better performance***
    
    ***Add new feature for user authentication***
    
    ***Update UI for better user experience***
    
    ***Fix typo in variable name***
    
    ***Optimize database queries***
    
    ***Add error handling for edge cases***
    
    ***Update dependencies to latest versions***
    
    ***Remove unused code***
    
    ***Improve code readability and maintainability

---
+++ src/Raffle.sol
@@ -0,0 +1,25 @@
+//SPDX License Identifier: MIT
+
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.22;
+
+/**
+ * @title  Raffle Contract
+ * @author github.com/to4to
+ * @notice CReating A Raffle i.e. A Lottery Contract
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle {
+    function enterRaffle()  public {
+        
+    }
+
+
+    function pickWinner() public{
+
+    }
+
+
+
+}
