f532f82a9ecf1ff6dacc1642fb213339f97930f5
to4to <justjustifying@outlook.com>
2024-03-23 23:47:31 +0530

    Fix revert behavior and emit PickedWinner event in Raffle contract

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -107,12 +107,12 @@ contract Raffle is VRFConsumerBaseV2 {
 
         s_players = new address payable[](0);
         s_lastTimeStamp = block.timestamp;
+        emit PickedWinner(winner);
         (bool success, ) = winner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
         }
 
-        emit PickedWinner(winner);
     }
 
     /** Getter Function */
